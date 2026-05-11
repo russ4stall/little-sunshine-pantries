79e281e1d86cbff833261126a28fa3f7fa56cb0b
russ4stall <russ@slashpine.tech>
2026-05-11 11:36:41 -0400

    Embedded food request form

---
+++ src/pages/request-food-delivery.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from '@/components/layout';
+
+const RequestFoodDeliveryPage = () => {
+  return (
+    <Layout>
+      <section className="bg-satin-linen-200">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <iframe className="airtable-embed" src="https://airtable.com/embed/appX16lmy5hv0Qto5/paglVGoWgVtVO7v5T/form" frameborder="0" onmousewheel="" width="100%" height="1400" style={{background: "transparent", border: "1px solid #ccc", overflow: "hidden"}}></iframe>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default RequestFoodDeliveryPage;
